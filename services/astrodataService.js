3fb77ab9d56aba9687167d1427910e29a48967a4
gerolvr <gerolvr@gmx.com>
2017-11-01 17:58:14 +1100

    Fix for the url in the astro data search

--- services/astrodataService.js
+++ services/astrodataService.js
@@ -1,9 +1,9 @@
 var myApp = angular.module('StargazerDiaryAngularJsApp');
 // var myApp = angular.module('StargazerDiaryAngularJsApp');
-myApp.service('AstrodataService', function ($http) {
+myApp.service('AstrodataService', function ($http, baseUrl) {
 
     this.newSearch = function(astronomicalObjectName) {
-        return $http.get('http://localhost:8080/api/v1/astrodata/search/' + astronomicalObjectName, {
+        return $http.get(baseUrl + '/astrodata/search/' + astronomicalObjectName, {
             headers: {
                     'x-auth-token': localStorage.getItem('SGD_AJS_xAuthToken')
             }
